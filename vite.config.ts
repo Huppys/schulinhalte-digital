0a9542bd79d6e16d4311f6eea0fe310164a74dca
Oliver Witzki <oliver.witzki@chrono24.com>
2024-04-26 23:44:46 +0200

    Upgrade dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,33 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import VueDevTools from "vite-plugin-vue-devtools";
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-   base: '/',
-   appType: 'spa',
-   publicDir: 'public',
-   plugins: [
-      vue()
-   ],
-   assetsInclude: [
-      'assets/*'
-   ],
-   resolve: {
+export default defineConfig(({ command }) => {
+
+  const plugins = [
+    vue()
+  ];
+
+  if (command === "serve") {
+    plugins.push(VueDevTools());
+  }
+
+  return {
+    base: "/",
+    appType: "spa",
+    publicDir: "public",
+    plugins: plugins,
+    assetsInclude: [
+      "assets/*"
+    ],
+    resolve: {
       alias: {
-         '@': fileURLToPath(new URL('./src', import.meta.url))
+        "@": fileURLToPath(new URL("./src", import.meta.url))
       }
-   }
+    }
+  };
 });
